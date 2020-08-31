cd320009d5380bb46b2e862bb2a285765c5e9195
alizahir@live.com <alizahir@live.com>
2020-08-30 22:26:23 -0400

    Changes config

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+// Access Env. variables
+dotenv.config()
 
 const app = express()
 
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send('Hello Universe!')
 })
 
 app.listen(port, () => console.log(`listening at 3000`))
